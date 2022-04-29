a44e18ac3858e14c39c70c55359227e160ca6430
leehyunwoo <qhqh0001@gmail.com>
2022-04-29 11:07:08 +0900

    폰트문제 해결

--- js/counter.js
+++ js/counter.js
@@ -5,7 +5,8 @@ const int = document.querySelector('.count');
 let integer = 1;
 
 // 합계금액
-const price = document.querySelector('.price');
+// const price = document.querySelector('.price');
+const priceValue = document.getElementById('price').value=25600;
 const totalPrice = document.querySelector('.total_price');
 const controller = document.querySelector('.controller_wrap');
 
@@ -13,15 +14,18 @@ window.onload = function () {
   add.addEventListener('click', function () {
     integer += 1;
     int.innerHTML = integer;
+
+
     //구매 수량
-    Number(int);
+    let aa = Number(integer);
     //1개당 가격
-    // Number(price);
-    var lastprice = parseInt(price);
-    totalPrice.innerHTML= int * price;
+    let bb = Number(price);
+    let cc = Number(priceValue);
+    totalPrice.innerHTML = aa * priceValue;
 
-
-    console.log(typeof int);
+    console.log(aa);
+    console.log(typeof aa);
+    console.log(priceValue);
   });
 
   remove.addEventListener('click', function () {
@@ -40,8 +44,8 @@ window.onload = function () {
 
 //수량에 변화가 생긴다면, 수량과 금액을 더하세요.
 function sum() {
-  if (controller.click  == true) {
+  if (controller.click == true) {
     console.log(aa);
-    totalPrice.innerHTML= (int * price)+`원`;
+    totalPrice.innerHTML = (int * price) + `원`;
   }
 }
