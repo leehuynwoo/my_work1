c92e29a2218efb842c5901fb21543057918f99b9
leehyunwoo <qhqh0001@gmail.com>
2022-04-29 11:45:39 +0900

    -

--- js/counter.js
+++ js/counter.js
@@ -6,6 +6,18 @@ let integer = 1;
 
 // 합계금액
 // const price = document.querySelector('.price');
+
+// 질문! 
+// <p id="price" class="price" type="text" name="amout" value="25600">25600</p> <p>원</p>
+
+const priceValue11 = document.getElementById('price').value // 질문 1=> 데이터가 value가 안 나오고 undefined로 찍히는 이유가 뭘까요 ㅠ..?
+let bb = Number(price); // 질문 2 => number로 감싼 후 console.log 찍어도 Nan로 나오나요? typeof로 찍으면 Number로 나옴
+console.log('질문1은' + priceValue11 + '입니다'); // 질문1은undefined입니다
+console.log('질문1의 타입은' + typeof priceValue11 + '입니다'); // 질문1의 타입은undefined입니다
+console.log('질문 2 bb는' + bb + '입니다'); // 질문 2 bb는NaN입니다
+console.log('질문 2 bb의 타입은' + typeof bb + '입니다') //질문 2 bb의 타입은number입니다
+
+
 const priceValue = document.getElementById('price').value=25600;
 const totalPrice = document.querySelector('.total_price');
 const controller = document.querySelector('.controller_wrap');
@@ -19,13 +31,12 @@ window.onload = function () {
     //구매 수량
     let aa = Number(integer);
     //1개당 가격
-    let bb = Number(price);
+    
     let cc = Number(priceValue);
     totalPrice.innerHTML = aa * priceValue;
 
     console.log(aa);
-    console.log(typeof aa);
-    console.log(priceValue);
+    console.log(typeof aa);;
   });
 
   remove.addEventListener('click', function () {
@@ -38,14 +49,3 @@ window.onload = function () {
   });
 
 };
-
-//합계금액
-
-
-//수량에 변화가 생긴다면, 수량과 금액을 더하세요.
-function sum() {
-  if (controller.click == true) {
-    console.log(aa);
-    totalPrice.innerHTML = (int * price) + `원`;
-  }
-}
